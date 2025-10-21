9a9143e33dcb428661ec7076941af53e622e5d95
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2025-10-21 16:55:36 +0400

    rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Head from 'next/head';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -17,6 +18,9 @@ export { viewport } from './[locale]/layout';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning>
+      <Head>
+        <meta name="google-site-verification" content="S18uHqVANxuK6blh-HlRI_xEzlrKZOv0_hsueZQKsB4" />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
