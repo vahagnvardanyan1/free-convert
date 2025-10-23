18254853a570fe033e1b269b9f9261631c4105b9
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2025-10-23 13:26:23 +0400

    google analytics

--- src/components/GoogleAnalytics/index.tsx
+++ src/components/GoogleAnalytics/index.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-// import { usePathname } from 'next/navigation';
-// import { useEffect } from 'react';
-// import { GoogleAnalytics } from '@next/third-parties/google';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
-// const GA_MEASUREMENT_ID = 'G-1CGMG9N67C';
+const GA_MEASUREMENT_ID = 'G-PK5E4KQX15';
 
 const Analytics = () => {
-  // const pathname = usePathname();
-  // useEffect(() => {
-  //   window.gtag?.('event', 'page_view', { page_path: pathname });
-  // }, [pathname]);
-  // return <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />;
+  const pathname = usePathname();
+  useEffect(() => {
+    window.gtag?.('event', 'page_view', { page_path: pathname });
+  }, [pathname]);
+  return <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />;
 };
 
 export default Analytics;
